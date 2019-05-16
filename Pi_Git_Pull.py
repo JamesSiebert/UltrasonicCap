17a9884e35bea71d591da87aa35c835deae8b24b
James Siebert <40009834+JamesSiebert@users.noreply.github.com>
2019-05-16 15:03:21 +1000

    Initial Commit

---
+++ Pi_Git_Pull.py
@@ -0,0 +1,31 @@
+import os
+import time
+# Project UltrasonicCap
+
+localDir = '/home/pi/UltrasonicCap/'
+git = 'https://github.com/JamesSiebert/UltrasonicCap.git'
+push_and_pull = False
+
+# If this script allows Git Push
+print('Auto Pull Project')
+if push_and_pull:
+    command = input('enter "push" OR "pull"')
+else:
+    command = 'pull'
+
+# Pull Command
+if command == 'pull':
+    print('Git Pull Started')
+    os.system("cd " + localDir)
+    os.system("git pull origin master")
+    print('Git Pull Complete')
+
+# Push Command
+if command == 'push':
+    print('Git Push Started')
+    os.system("cd " + localDir)
+    os.system("git push origin master")
+    print('Git Push Complete')
+
+time.sleep(5)
+print('COMPLETE')
