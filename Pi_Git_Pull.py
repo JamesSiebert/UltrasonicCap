e10a78a77e2152e4c35557921f5293ee6980ee8c
James Siebert <admin@media360.com.au>
2019-05-16 15:45:48 +1000

    Initial Commit 4

--- Pi_Git_Pull.py
+++ Pi_Git_Pull.py
@@ -4,10 +4,7 @@ import time
 # Project UltrasonicCap
 
 # Pi
-# localDir = '/home/pi/UltrasonicCap/'
-
-# PC
-localDir = 'C:/_griffith/Robotics/UltrasonicCap'
+localDir = '/home/pi/UltrasonicCap'
 
 git = 'https://github.com/JamesSiebert/UltrasonicCap.git'
 push_and_pull = False
@@ -22,14 +19,14 @@ else:
 # Pull Command
 if command == 'pull':
     print('Git Pull Started')
-    os.system("cd " + localDir)
+    # os.system("cd " + localDir)
     os.system("git pull origin master")
     print('Git Pull Complete')
 
 # Push Command
 if command == 'push':
     print('Git Push Started')
-    os.system("cd " + localDir)
+    # os.system("cd " + localDir)
     os.system("git push origin master")
     print('Git Push Complete')
 
