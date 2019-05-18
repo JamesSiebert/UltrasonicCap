433e91c6b8a20dd6cd7088b3f5c382da8930ab31
James Siebert <admin@media360.com.au>
2019-05-18 23:20:10 +1000

    test webcam

---
+++ c922_cam_test.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+from pygame.locals import *
+import pygame.camera
+pygame.init()
+pygame.camera.init()
+cam = pygame.camera.Camera("/dev/video0", (352, 288))
+cam.start()
+image = cam.get_image()
+pygame.image.save(image, '/tmp/101.bmp')
+cam.stop()
